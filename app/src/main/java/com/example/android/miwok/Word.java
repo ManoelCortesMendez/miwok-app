4491d586532222df4ea0374505441cb70a402080
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-11-12 17:39:41 +0100

    Add audio: pronunciation of words

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,16 +5,19 @@ public class Word {
     private String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1;
+    private int mAudioResourceId;
 
-    public Word(String aDefaultTranslation, String aMiwokTranslation) {
+    public Word(String aDefaultTranslation, String aMiwokTranslation, int anAudioResourceId) {
         mDefaultTranslation = aDefaultTranslation;
         mMiwokTranslation = aMiwokTranslation;
+        mAudioResourceId = anAudioResourceId;
     }
 
-    public Word(String aDefaultTranslation, String aMiwokTranslation, int anImageResourceId) {
+    public Word(String aDefaultTranslation, String aMiwokTranslation, int anImageResourceId, int anAudioResourceId) {
         mDefaultTranslation = aDefaultTranslation;
         mMiwokTranslation = aMiwokTranslation;
         mImageResourceId = anImageResourceId;
+        mAudioResourceId = anAudioResourceId;
     }
 
     public String getDefaultTranslation() {
@@ -28,4 +31,16 @@ public class Word {
     public int getImageResourceId() { return mImageResourceId; }
 
     public boolean hasImage() { return mImageResourceId != NO_IMAGE_PROVIDED; }
+
+    public int getmAudioResourceId() { return mAudioResourceId; }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mAudioResourceId=" + mAudioResourceId +
+                '}';
+    }
 }
